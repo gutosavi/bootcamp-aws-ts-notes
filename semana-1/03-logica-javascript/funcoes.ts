714ace9825aebf7043744b5969712661a41c8374
Gustavo Savi <gutosavi@hotmail.com>
2026-09-10 09:18:27 -0300

    feat: exercicio função calcular usando switch

--- semana-1/03-logica-javascript/funcoes.ts
+++ semana-1/03-logica-javascript/funcoes.ts
@@ -11,3 +11,23 @@ const areaCircunferencia = (raio: number): number => {
   return Number(resultado.toFixed(2));
 };
 console.log(areaCircunferencia(3));
+
+const calcular = (
+  n1: number,
+  n2: number,
+  operacao: string,
+): number | string => {
+  switch (operacao) {
+    case "+":
+      return n1 + n2;
+    case "-":
+      return n1 - n2;
+    case "*":
+      return n1 * n2;
+    case "/":
+      return n1 / n2;
+    default:
+      return "Operador não cadastrado";
+  }
+};
+console.log(calcular(10, 10, "/"));
