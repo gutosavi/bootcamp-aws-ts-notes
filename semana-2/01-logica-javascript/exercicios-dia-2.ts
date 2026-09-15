c89696adc9ceeb6828b8acd1292db991064bcbfc
Gustavo Savi <gutosavi@hotmail.com>
2026-09-15 19:02:48 -0300

    feat: exercício de mapeamento e filtro de dados

--- semana-2/01-logica-javascript/exercicios-dia-2.ts
+++ semana-2/01-logica-javascript/exercicios-dia-2.ts
@@ -20,3 +20,36 @@ const formatarUsuario = (
   };
 };
 console.log(formatarUsuario("GUSTAVO", "gustAVO@mail.com"));
+console.clear();
+
+// Filtro e Mapeamento de Produtos
+
+type Produto = {
+  id: number;
+  nome: string;
+  preco: number;
+  emEstoque: boolean;
+};
+
+const produtos: Produto[] = [
+  { id: 1, nome: "Teclado Mecânico", preco: 250, emEstoque: true },
+  { id: 2, nome: "Mouse Gamer", preco: 120, emEstoque: false },
+  { id: 3, nome: "Monitor 24", preco: 900, emEstoque: true },
+  { id: 4, nome: "Headset", preco: 180, emEstoque: true },
+];
+
+const produtosComDesconto = (produtos: Produto[]): {} => {
+  return produtos
+    .filter((produto) => produto.emEstoque === true)
+    .map((produto) => {
+      const nome = produto.nome;
+      const preco = produto.preco;
+
+      return {
+        nome,
+        precoComDesconto: preco * 0.9,
+      };
+    });
+};
+console.log(produtosComDesconto(produtos));
+console.clear();
