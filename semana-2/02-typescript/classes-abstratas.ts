a2f5085fdfcdb57cb4a1fe1e250f9174b1f2ca40
Gustavo Savi <gutosavi@hotmail.com>
2026-09-16 20:29:09 -0300

    feat: cria classe com atributos abstratos

---
+++ semana-2/02-typescript/classes-abstratas.ts
@@ -0,0 +1,64 @@
+// Classes abstratas
+
+export abstract class Personagem {
+  constructor(
+    protected nome: string,
+    protected ataque: number,
+    protected vida: number,
+  ) {}
+
+  atacar(personagem: Personagem): void {
+    if (personagem.vida <= 0) {
+      console.log(`${personagem.nome} está morto. Não pode mais ser atacado.`);
+      return;
+    }
+
+    console.log(`O ${this.nome} está atacando...`);
+    this.bordao();
+    personagem.perderVida(this.ataque);
+  }
+
+  perderVida(forcaAtaque: number): void {
+    if (this.vida <= 0) {
+      console.log(`${this.nome} morreu.`);
+      return;
+    }
+
+    console.log(`O ${this.nome} está com ${this.vida} vida agora.`);
+    this.vida -= forcaAtaque;
+  }
+
+  bordao(): void {
+    console.log(`ATACANDOOO...`);
+  }
+}
+
+export class Guerreiro extends Personagem {
+  override bordao(): void {
+    console.log(`${this.nome} ATACANDOOOO...`);
+  }
+}
+export class Monstro extends Personagem {
+  override bordao(): void {
+    console.log(`Eu sou o ${this.nome} - O MAIS FORTEEEE!`);
+  }
+}
+
+const guerreiro = new Guerreiro("Guerreiro", 95, 1000);
+const monstro = new Monstro("Monstro", 100, 1500);
+
+guerreiro.atacar(monstro);
+guerreiro.atacar(monstro);
+guerreiro.atacar(monstro);
+console.log("-----");
+monstro.atacar(guerreiro);
+monstro.atacar(guerreiro);
+monstro.atacar(guerreiro);
+monstro.atacar(guerreiro);
+monstro.atacar(guerreiro);
+monstro.atacar(guerreiro);
+monstro.atacar(guerreiro);
+monstro.atacar(guerreiro);
+monstro.atacar(guerreiro);
+monstro.atacar(guerreiro);
+monstro.atacar(guerreiro);
